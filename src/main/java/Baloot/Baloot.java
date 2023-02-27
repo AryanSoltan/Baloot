3381a673da838658edd550824d5f52df35be5f64
sabashv <sabashahsavari01@gmail.com>
2023-02-27 23:08:35 +0330

    add commands

--- src/main/java/Baloot/Baloot.java
+++ src/main/java/Baloot/Baloot.java
@@ -4,7 +4,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
 import java.util.Scanner;
-
+import java.util.*;
 public class Baloot {
 
     static final int TYPE_COMMAND_INDEX = 0;
@@ -12,6 +12,19 @@ public class Baloot {
     static final String ADD_USER_COMMAND = "addUser";
 
     static final String ADD_PROVIDER_COMMAND = "addProvider";
+
+    static final String ADD_COMMODITY = "addCommodity";
+
+    static final String GET_COMMODITIES_LIST="getCommoditiesList";
+    static final String RATE_COMMODITY="rateCommodity";
+    static final String ADD_TO_BUYLIST="addToBuyList";
+
+    static final String REMOVE_FROM_BUYLIST="removeFromBuyList";
+
+    static final String GET_COMMODITY_BY_ID="getCommodityById";
+
+    static final String GET_COMMODITIES_BY_Category="getCommoditiesByCategory";
+    static final String GET_BUY_LIST="fetBuyList";
 
     //static final String SUCCESSFUL = "addUser";
     static final int MAX_PARSE_COMMAND = 2;
@@ -43,8 +56,11 @@ public class Baloot {
         {
             String new_command = inputReader.nextLine();
             String[] command_parsed = new_command.split(" ", MAX_PARSE_COMMAND);
-            parseCommand(command_parsed[Baloot.TYPE_COMMAND_INDEX],
-                    command_parsed[Baloot.JSON_INDEX]);
+            String data="{}";
+            if(command_parsed.length == MAX_PARSE_COMMAND)
+                data = command_parsed[Baloot.JSON_INDEX];
+
+             parseCommand(command_parsed[Baloot.TYPE_COMMAND_INDEX], data);
         }
     }
 
@@ -52,7 +68,9 @@ public class Baloot {
     {
         JSONParser jsonCommand = new JSONParser();
         try {
+
             JSONObject jsonParser = (JSONObject)jsonCommand.parse(json_input);
+
             switch(command_input)
             {
                 case Baloot.ADD_USER_COMMAND:
@@ -62,10 +80,34 @@ public class Baloot {
                 case Baloot.ADD_PROVIDER_COMMAND:
                     addProvider(jsonParser);
                     break;
+                case Baloot.ADD_COMMODITY:
+                    addCommodity(jsonParser);
+                    break;
+                case Baloot.GET_COMMODITIES_LIST:
+                    getCommoditiesList();
+                    break;
+                case Baloot.RATE_COMMODITY:
+                    rateCommodity(jsonParser);
+                    break;
+                case Baloot.ADD_TO_BUYLIST:
+                    addToBuyList(jsonParser);
+                    break;
+                case Baloot.REMOVE_FROM_BUYLIST:
+                    removeFromBuyList(jsonParser);
+                    break;
+                case Baloot.GET_COMMODITY_BY_ID:
+                    getCommodityById(jsonParser);
+                    break;
+                case Baloot.GET_COMMODITIES_BY_Category:
+                    getCommoditiesByCategory(jsonParser);
+                    break;
+                case Baloot.GET_BUY_LIST:
+                    getBuyList(jsonParser);
+                    break;
             }
         }
         catch (ParseException e) {
-            System.out.println(e.getMessage());
+            System.out.println("hehe");
         }
     }
 
@@ -77,7 +119,8 @@ public class Baloot {
         String email = (String)jsonParser.get("email");
         String birthDate = (String)jsonParser.get("birthDate");
         String address = (String)jsonParser.get("address");
-        double credit = (double)jsonParser.get("credit");
+        double credit = ((Number)jsonParser.get("credit")).doubleValue();
+
 
         //System.out.println("hihi");
         if(balootDatabase.doesExist(name)) {
@@ -92,12 +135,78 @@ public class Baloot {
 
     private static void addProvider(JSONObject jsonParser) {
 
-        int id = (int) jsonParser.get("id");
+        int id = ((Number) jsonParser.get("id")).intValue();
         String name = (String)jsonParser.get("name");
         String date = (String) jsonParser.get("registryDate");
         balootDatabase.addProvider(id, name, date);
         printOutput(true, "Provider " + name + " added");
     }
+
+    private static void addCommodity(JSONObject jsonParser) {
+
+        int id = ((Number) jsonParser.get("id")).intValue();
+        String name = (String)jsonParser.get("name");
+        int providerId = ((Number) jsonParser.get("providerId")).intValue();
+        double price = ((Number) jsonParser.get("price")).doubleValue();
+
+        String categories = (String)jsonParser.get("categories");
+        double rating = ((Number) jsonParser.get("rating")).doubleValue();
+        int inStock = ((Number) jsonParser.get("inStock")).intValue();
+       // balootDatabase.addCommodity(id, name, providerId,price,categories, rating,inStock);
+        printOutput(true, "Commodity added");
+    }
+
+    private static void getCommoditiesList() {
+
+     //   String data = balootDatabase.getCommoditiesList();
+       // printOutput(true, data);
+    }
+
+    private static void rateCommodity(JSONObject jsonParser) {
+
+
+        String username = (String)jsonParser.get("username");
+        int commodityId = ((Number) jsonParser.get("commodityId")).intValue();
+        double score = ((Number) jsonParser.get("score")).doubleValue();
+       // todo
+    }
+
+    private static void addToBuyList(JSONObject jsonParser) {
+
+
+        String username = (String)jsonParser.get("username");
+        int commodityId = ((Number) jsonParser.get("commodityId")).intValue();
+
+        // todo
+    }
+
+    private static void removeFromBuyList(JSONObject jsonParser) {
+
+
+        String username = (String)jsonParser.get("username");
+        int commodityId = ((Number) jsonParser.get("commodityId")).intValue();
+
+        // todo
+    }
+
+    private static void getCommodityById(JSONObject jsonParser) {
+        int commodityId = ((Number) jsonParser.get("id")).intValue();
+
+        // todo
+    }
+
+    private static void getCommoditiesByCategory(JSONObject jsonParser) {
+        String category = (String)jsonParser.get("category");
+
+        // todo
+    }
+
+    private static void getBuyList(JSONObject jsonParser) {
+        String username = (String)jsonParser.get("username");
+
+        // todo
+    }
+
     public static void main(String[] args) {
         run();
     }
