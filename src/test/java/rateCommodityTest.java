234528190bd98cc857ce9962ca2b7bd6139d9c52
sabashv <sabashahsavari01@gmail.com>
2023-02-28 15:32:35 +0330

    start test

---
+++ src/test/java/rateCommodityTest.java
@@ -0,0 +1,15 @@
+import Baloot.Baloot;
+import Baloot.BalootServer;
+import org.junit.BeforeClass;
+
+public class rateCommodityTest {
+    private Baloot balootInterface;
+    @BeforeClass
+    public void setup() {
+        balootInterface = new Baloot();
+        balootInterface.add
+
+
+    }
+
+}
