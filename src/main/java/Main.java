ed8881e2b17cecb729fe122e35100a7ab313d82b
sabashv <sabashahsavari01@gmail.com>
2023-03-11 16:22:18 +0330

    add connection to external server

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import Baloot.BalootServer;
+import InterfaceServer.InterfaceServer;
+
+public class main {
+    private static InterfaceServer interfaceServer;
+    private static BalootServer balootServer;
+}
